cdd7d03c1b3398dc64066daac8f12f363452d688
Keaton Bonomo <kbonomo28@gmail.com>
2021-07-11 13:50:23 -0500

    recruision module - reverse string char array

---
+++ src/main/java/applications/recursionpractice/ReverseString.java
@@ -0,0 +1,27 @@
+package applications.recursionpractice;
+
+public class ReverseString {
+
+    // This one uses another array
+    public char[] reverseString(char[] s){
+        int len = s.length;
+        char[] temp = new char[len];
+        int count = 0;
+        for (int i = len-1; i >= 0 ; i--) {
+            temp[count] = s[i];
+            count++;
+        }
+        return temp;
+    }
+
+    // TODO: This is a basic one, come back to study this
+    public char[] reverseSameStrings(char[] s){
+        int len = s.length;
+        for (int i = 0; i < len / 2 ; i++) {
+            char ch = s[i];
+            s[i] = s[len - i -1];
+            s[len - i - 1] = ch;
+        }
+        return s;
+    }
+}
