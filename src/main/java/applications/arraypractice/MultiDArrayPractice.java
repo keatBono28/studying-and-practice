5d0fc79703bf8831b08e590b16b587e58e4aee73
Keaton Bonomo <kbonomo28@gmail.com>
2021-07-19 10:53:27 -0500

    Spiral Traversing through matrix, notes and solution

--- src/main/java/applications/arraypractice/MultiDArrayPractice.java
+++ src/main/java/applications/arraypractice/MultiDArrayPractice.java
@@ -1,5 +1,8 @@
 package applications.arraypractice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiDArrayPractice {
     private static void printArray(int[][] a) {
         for (int i = 0; i < a.length; ++i) {
@@ -68,6 +71,180 @@ public class MultiDArrayPractice {
 
 
     }
+
+    /**
+     *  Just committing my first try, then the actual solution, then a third method with notes
+     *  and thoughts to help explain how to find the solution
+     *
+     */
+
+    public List<Integer> spiralOrder(int[][] matrix){
+        /*
+        *  This was my first attempt without help... it failed and was bad
+        * */
+
+        if(matrix.length == 0){
+            return new ArrayList<>();
+        }
+
+        int rowCount = matrix.length;
+        int columnCount = matrix[0].length;
+        int currentRow = 0;
+        int currentColumn = 0;
+
+        List<Integer> resultList = new ArrayList<>();
+
+        int rowTimesColumn = columnCount * rowCount;
+
+        for (int i = 0; i < rowTimesColumn ; i++) {
+            resultList.add(matrix[currentRow][currentColumn]);
+            if(currentColumn < columnCount && (rowCount - currentRow) != 0){
+                currentColumn++;
+            } else if(currentColumn == columnCount){
+                currentRow++;
+            } else if(currentRow == rowCount){
+                currentColumn--;
+            } else{
+                break;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Integer> spiral(int[][] matrix){
+        List<Integer> result = new ArrayList<>();
+        int rowBegin = 0, columnBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int columnEnd = matrix[0].length - 1;
+
+        while(rowBegin <= rowEnd && columnBegin <= columnEnd){
+            for (int i = columnBegin; i <= columnEnd ; i++) {
+                result.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+            for (int i = rowBegin; i <= rowEnd ; i++) {
+                result.add(matrix[i][columnEnd]);
+            }
+            columnEnd--;
+            if(rowBegin <= rowEnd){
+                for (int i = columnEnd; i >= columnBegin ; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--;
+            if(columnBegin <= columnEnd){
+                for (int i = rowEnd; i >= rowBegin ; i--) {
+                    result.add(matrix[i][columnBegin]);
+                }
+            }
+            columnBegin++;
+        }
+        return result;
+    }
+
+    public List<Integer> spiralTraversingPractice(int[][] matrix){
+        // First make a list to return the result, we can't alter the existing matrix
+        List<Integer> result = new ArrayList<>();
+
+        // Now we need to make a few variables,
+        // Lets find the beginning row and beginning column, we know they will be 0,0
+        int rowBegin = 0, columnBegin = 0;
+        // Now we need to find where the row and columns end, sort of like finding a grid system
+        /* Putting this here so i can see a visual
+        *
+           [[ 1 , 2 , 3 , 4 ],
+        *   [ 5 , 6 , 7 , 8 ],
+        *   [ 9 , 5 , 1 , 3 ]]
+        *
+        */
+        int rowEnd = matrix.length - 1;
+        int columnEnd = matrix[0].length - 1;
+
+        // Now we a while loop to start traversing
+        // lets set our boundaries, we know that once these become false, then we 
+        // have reached the center of the matrix spiral
+        while(rowBegin <= rowEnd && columnBegin <= columnEnd){
+            // Step 1 - go across the first row
+            // use columnBegin and columnEnd to go across
+            for (int i = columnBegin; i <= columnEnd ; i++) {
+                // Now add the first row to the end result
+                result.add(matrix[rowBegin][i]);
+            }
+            // Now we went across the first row, and we need to "eliminate" that row
+            // basically meaning, increase rowBegin because we already used it, so next time we
+            // get to the row, it will be on the next row
+            rowBegin++;
+            
+            // Step 2 - Now we need to go down the last column
+            // use the rowBegin and rowEnd to start
+            for (int i = rowBegin; i < rowEnd ; i++) {
+                // Now add the last column to the result
+                result.add(matrix[i][columnEnd]);
+            }
+            // Now that we went down the last column, we need to "eliminate" that column
+            // basically meaning, decrease the columnEnd beause we already used it, so next time we
+            // get to the end boundry, we have the next column
+            columnEnd--;
+
+            // Step 3 - Now that we are at the bottom corner of the matrix, we need to iterate backwards
+            // on that bottom row, back to the beginning column
+            if(rowBegin <= rowEnd){
+                // Now iterate backwards, using the columnEnd and move towards columnBegin
+                for (int i = columnEnd; i >= columnBegin ; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+            }
+            // Now that we iterated backwards through the last available row, we need to decrease the
+            // value of the rowEnd
+            rowEnd--;
+
+            // Step 4 - Now that we are back at the beginning column, we need to go up
+            //
+            if(columnBegin <= columnEnd){
+                for (int i = rowEnd; i >= rowBegin ; i--) {
+                    result.add(matrix[i][columnBegin]);
+                }
+            }
+            // Now that we went up the beginning column, we need to increase it
+            columnBegin++;
+
+            // Now we have done a full "circle", if the boundaries above are still true, we will loop back through
+            // the next spiral
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
